dbffb4986cdb4d1137ad3bf1a2cc05afee54c6e4
Umid Olimzhanov <uolimzhanov@gmail.com>
2024-08-12 11:04:14 +0500

    some enhancements

--- app/src/main/java/com/uolimzhanov/navigationresult/MainActivity.kt
+++ app/src/main/java/com/uolimzhanov/navigationresult/MainActivity.kt
@@ -6,9 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -54,28 +57,10 @@ class MainActivity : ComponentActivity() {
                         composable<Screens.ResultReturningRoute> { entry ->
                             val args = entry.toRoute<Screens.ResultReturningRoute>()
 
-                            val parentViewModel = when (args.previousScreenName) {
-                                "Main" -> {
-                                    val parentEntry = remember(entry) {
-                                        navController.getBackStackEntry<Screens.MainRoute>()
-                                    }
-                                    hiltViewModel<MainViewModel>(parentEntry)
-                                }
-
-                                "Second" -> {
-                                    val parentEntry = remember(entry) {
-                                        navController.getBackStackEntry<Screens.SecondScreen>()
-                                    }
-                                    hiltViewModel<SecondViewModel>(parentEntry)
-                                }
-
-                                else -> {
-                                    val parentEntry = remember(entry) {
-                                        navController.getBackStackEntry<Screens.MainRoute>()
-                                    }
-                                    hiltViewModel<MainViewModel>(parentEntry)
-                                }
-                            }
+                            val parentViewModel = navController.getViewModel(
+                                screenName = args.previousScreenName,
+                                entry = entry
+                            )
 
                             ScreenWithResultRoot(
                                 onGoBack = { username ->
@@ -100,4 +85,33 @@ sealed interface Screens {
 
     @Serializable
     data class ResultReturningRoute(val previousScreenName: String) : Screens
+}
+
+@Composable
+fun NavController.getViewModel(
+    screenName: String,
+    entry: NavBackStackEntry
+): BaseViewModel {
+    return when (screenName) {
+        "Main" -> {
+            val parentEntry = remember(entry) {
+                getBackStackEntry<Screens.MainRoute>()
+            }
+            hiltViewModel<MainViewModel>(parentEntry)
+        }
+
+        "Second" -> {
+            val parentEntry = remember(entry) {
+                getBackStackEntry<Screens.SecondScreen>()
+            }
+            hiltViewModel<SecondViewModel>(parentEntry)
+        }
+
+        else -> {
+            val parentEntry = remember(entry) {
+                getBackStackEntry<Screens.MainRoute>()
+            }
+            hiltViewModel<MainViewModel>(parentEntry)
+        }
+    }
 }
